b57872528889b5f553f054f8ae839fecf67e4365
isaacfavel <pirrisfavela@gmail.com>
2025-10-28 16:56:45 -0600

    Agregar archivos de proyecto.

---
+++ MauiApp01/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp01")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp01.Pages")]
